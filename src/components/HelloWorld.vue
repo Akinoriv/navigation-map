b29c191c69b691d97930d5cc0ccf68365da84af2
akinoriv <nika.nika1711@gmail.com>
2020-04-20 22:34:32 +0300

    канвас с точкой

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,9 @@
   <div>
     <h2>Координаты вашей точки</h2>
     <a>  {{trilatiratiom2d()}} </a>
+    {{test()}}
   </div>
+  <div id="coords">(координаты покажутся здесь)</div>
   <canvas id='example'>Canvas</canvas>
 </div> 
 </template>
@@ -30,20 +32,21 @@ export default {
     return {
       x : {
         a: 0,
-        b: 18,
-        c: 7.5
+        b: 160,
+        c: 65
      },
      y : {
         a: 0,
         b: 0,
-        c: 16
+        c: 60
       },
       z : {
-        a: 28.5,
+        a: 265,
         b: 0,
-        c: 27
-     },
-     
+        c: 210
+      },
+      q:[],
+      w:[],
       ctx: null,  
       
     }
@@ -53,53 +56,79 @@ export default {
   
   methods: {
    
-    updateCanvas: function() {
+    // updateCanvas: function() {
+    //   var canvas = document.getElementById("example");
+    //   canvas.height = 480;
+    //   canvas.width  = 640;
+
+    //   var ctx = canvas.getContext('2d');
+   
+    //   //var img = new Image(); 
+    //   //var img = document.getElementById("123");
+    //   var img = new Image(); 
+    //   img.src = "https://avatars.mds.yandex.net/get-pdb/1870458/3559d035-2493-4396-aa16-696954177fd0/s1200";
+    //   img.onload = function() {    
+    //     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    //   }
+    //   // ctx.beginPath();
+		// 	// ctx.moveTo(10, 15);
+		// 	// ctx.bezierCurveTo(75, 55, 175, 20, 250, 15);
+		// 	// ctx.moveTo(10, 15);
+		// 	// ctx.quadraticCurveTo(100, 100, 250, 15);
+    //   // ctx.stroke(); 
+    // },
+
+    updateCanva: function() {
       var canvas = document.getElementById("example");
+      canvas.height = 160;
+      canvas.width  = 256;
       this.ctx = canvas.getContext('2d');
+      this.ctx.strokeRect(0, 0, 256, 160);
 
-      canvas.height = 480;
-			canvas.width  = 640;
-			// this.ctx.beginPath();
-			// this.ctx.moveTo(10, 15);
-			// this.ctx.bezierCurveTo(75, 55, 175, 20, 250, 15);
-			// this.ctx.moveTo(10, 15);
-			// this.ctx.quadraticCurveTo(100, 100, 250, 15);
-      // this.ctx.stroke();      
-      //var img = new Image(); 
-      var img = document.getElementById("123");
-      this.ctx.drawImage(img, 0, 0);
-      img.onload = function() {    
-        
-          // this.ctx.fillStyle = pattern;
-          // this.ctx.fillRect(1000, 1000, 300, 400);
-          // this.ctx.strokeRect(10, 10, 150, 150);
+      this.ctx.beginPath();
+      this.ctx.moveTo(0,0);
+      this.ctx.lineTo(15,0);
+      this.ctx.lineTo(0,15);
+      this.ctx.fill();
 
-        //var wRatio = canvas.width / img.width;
-        //var hRatio = canvas.height / img.height;
-        //var ratio = Math.min (wRatio, hRatio);
-        //alert(ratio)
-        //this.ctx.drawImage(img, 0, 0, canvas.width, canvas.height,0,0,img.width*ratio, img.height*ratio);
-        //this.ctx.drawImage(img, 0, 0, 200, 400)
-        //this.ctx.clearRect(0, 0, canvas.width, canvas.height);
-        this.ctx.drawImage(img, 0, 0);
-      }
+      this.ctx.moveTo(256,0);
+      this.ctx.lineTo(256,15);
+      this.ctx.lineTo(241,0);
+      this.ctx.fill();
+      
+      this.ctx.moveTo(0,160);
+      this.ctx.lineTo(0,145);
+      this.ctx.lineTo(15,160);
+      this.ctx.fill();
+    
+			this.ctx.moveTo(0, 0);
+			this.ctx.rect(this.q, this.w, 5, 5);
+      this.ctx.stroke(); 
     
     },
-//vironikaaaa
+// Подсчет координат
     trilatiratiom2d: function () {
       let q = []
       let w = []
       var p1 = -2 * this.x.a * w - 2 * this.x.b * q + Math.pow(this.x.a, 2) + Math.pow(this.x.b, 2) - Math.pow(this.x.c, 2)
       var p2 = -2 * this.y.a * w - 2 * this.y.b * q + Math.pow(this.y.a, 2) + Math.pow(this.y.b, 2) - Math.pow(this.y.c, 2)
       var p3 = -2 * this.z.a * w - 2 * this.z.b * q + Math.pow(this.z.a, 2) + Math.pow(this.z.b, 2) - Math.pow(this.z.c, 2)
-      w = (p1 - p2) / (2 * this.x.b)
-      q = -(p2 - p3) / (2 * this.z.a)
-      return ("координата х = " + q + ", координата y = " + w)
-      
-    } , 
+      this.w = (p1 - p2) / (2 * this.x.b)
+      this.q = -(p2 - p3) / (2 * this.z.a)
+      return ("координата х = " + this.q + ", координата y = " + this.w)
+    },
+// test
+    test:function () {
+    return ("координата х = " + this.q + ", координата y = " + this.w)
+    },
   },
+
+
+
+// Вывод канваса в интерфейс
   mounted: function () {
-    this.updateCanvas()
+    //this.updateCanvas(),
+    this.updateCanva()
     // тут функции при запуске страници которые будут выполняться
   }
 }
