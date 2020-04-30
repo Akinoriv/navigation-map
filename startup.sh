d8106643bbc1f56effa362d6c652238c871acdfd
Вироничка <49586977+Akinoriv@users.noreply.github.com>
2020-04-30 23:51:13 +0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,5 +7,4 @@ sudo apt update                 # обнавляю менеджер пакето
 yes Y | sudo apt install nodejs # скачиваю и устанавливаю нодеджс/ соглашаюсь со всем
 yes Y | sudo apt install npm    # скачиваю и устанавливаю  менеджер ракетов 
 npm install                     # идет в файл package.json и цстанавливает все зависимости 
-sudo npm install forever -g     # скачала и установила пакет форевер для авт работы
-forever start index.js          # запустила свою страничку
+npm run serve         # запустила свою страничку
